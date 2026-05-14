4171ba8b8898b6e508c4b8d4761c0373a33fdd6b
Anas Atthariq <anasatthariq0.2@gmail.com>
2026-05-14 15:35:39 +0700

    Update hasil.blade.php

--- resources/views/dashboard/hasil.blade.php
+++ resources/views/dashboard/hasil.blade.php
@@ -3,7 +3,13 @@
 @section('title', 'Data Pendaftar Beasiswa')
 
 @section('content')
-
+/*
+|--------------------------------------------------------------------------
+| Author      : Anas Atthariq
+| Tanggal     : 13 Mei 2026
+| Deskripsi   : Program dashboard pendaftaran beasiswa mahasiswa
+|--------------------------------------------------------------------------
+*/
 <div class="page-header fade-in-up d-flex align-items-start justify-content-between flex-wrap gap-3">
     <div>
         <h1><i class="fa-solid fa-table-list me-2" style="color:#818cf8"></i>Data Pendaftar Beasiswa</h1>
